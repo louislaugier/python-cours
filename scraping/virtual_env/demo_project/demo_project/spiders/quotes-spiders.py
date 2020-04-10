82a49b55df2a5c520bee27208fef473fd9f51b8f
louisl98 <louis.laugier@etu-webschoolfactory.fr>
2020-04-10 18:51:05 +0200

    update

--- scraping/virtual_env/demo_project/demo_project/spiders/quotes-spiders.py
+++ scraping/virtual_env/demo_project/demo_project/spiders/quotes-spiders.py
@@ -8,5 +8,12 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self,response):
-        self.logger.info("Working")
-        pass
+        # self.logger.info("Working")
+        # pass
+        quotes = response.css('div.quote')
+        for quote in quotes:
+            yield {
+                'text': quote.css('.text::text').get(),
+                'author': quote.css('.author::text').get(),
+                'tags': quote.css('.tag::text').getall()
+            }
